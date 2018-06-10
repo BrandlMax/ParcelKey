bdec9cdba6df4a1528f432c3ef82349309420dba
Domenika Tomaovic <domenika.tomasovic@stud.h-da.de>
2018-06-10 17:24:47 +0200

    Minigame nur der Anfang :))

---
+++ Minigame/MinigameParcelKey/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(805,300);
+    loadImage('img/hintergrund.svg', function(img) {
+        image(img, 0,0);
+    });
+    loadImage('img/postcar.png', function(img) {
+        image(img, 220,130);
+    });
+}
+
+function draw() {
+
+}
